d0a54e469a6ebf403882d105abee272615aeed51
BenAtic-KEA <mark024e@stud.kea.dk>
2021-10-06 11:27:32 +0200

    directory ændret på static filer

--- src/main/java/com/example/rokokopostenweb/Controller/PageController.java
+++ src/main/java/com/example/rokokopostenweb/Controller/PageController.java
@@ -2,12 +2,15 @@ package com.example.rokokopostenweb.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PageController {
 
     @GetMapping("/")
-    public String showHomepage(){
-        return "RokokoPosten";
+    public ModelAndView showHomepage(){
+            ModelAndView modelAndView = new ModelAndView();
+            modelAndView.setViewName("RokokoPosten");
+            return modelAndView;
     }
 }
